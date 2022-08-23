bae292820a8912a1a15042635e076d418c327b19
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-08-23 21:25:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/sh
 
 # Args
-# 1 - connection string
-# 2 - collection to add to
-# 3 - path to metrics file
+# 1 - collection to add to
+# 2 - path to metrics file
 
-if [ "$CONNECTION_STRING" = "" ]; then
-    echo "Database connection string not specified"
-    echo "1) Add your connection string as an env value"
-    echo "2) Set the connection-string property to the appropriate env variable"
-    echo "   E.g.,"
-    echo "   env:"
-    echo '     CONNECTION_STRING: ${{ secret.CONNECTION_STRING }}'
-    exit 1
-fi;
-
-metrics_path="/github/workspace/$3"
-python3 /upload_metrics.py "$CONNECTION_STRING" "$2" "$metrics_path"
+metrics_path="/github/workspace/$2"
+python3 /upload_metrics.py "$1" "$metrics_path"
