ea0d791f343b9232c023741295312d02b1f4bee1
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-08-24 08:38:06 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Args
+# 1 - connection string
 # 1 - collection to add to
 # 2 - path to metrics file
 
-if [ -z "${CONNECTION_STRING}" ]; then
-  echo "No [CONNECTION_STRING] set"
-  exit 1
-fi
-
-metrics_path="/github/workspace/$2"
-python3 /upload_metrics.py "${CONNECTION_STRING}" "$1" "$metrics_path"
+metrics_path="/github/workspace/$3"
+python3 /upload_metrics.py "$1" "$2" "$metrics_path"
